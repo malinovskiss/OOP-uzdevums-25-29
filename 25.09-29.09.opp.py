870918640f0209963dab2cd31bb04688b3415184
malinovskiss <90186408+malinovskiss@users.noreply.github.com>
2023-10-02 15:35:58 +0300

    Update 25.09-29.09.opp.py

--- 25.09-29.09.opp.py
+++ 25.09-29.09.opp.py
@@ -1,52 +1,58 @@
-#Tēma- Bibliotēkas vadības sistēma
+#Bibliotēkas vadības sistēma
+#Bibliotēkas importēšana
 import PySimpleGUI as psg
-#Klases izveide
+
+#Izveidota klase
 class Biblioteka():
-    # Konstruktora izveide
-    def __init__(self):
-        self.gramatu_kategorija = ""
-        self.gramatas_nosaukums = ""
-        self.gramatas_raksturojums = ""
-        self.gramatas_cena = ""
-        self.lasitaja_vards= ""
-        self.lasitaja_uzvards = ""
-        self.lasitaja_tel_nr = ""
-        self.lasitaja_pk = ""
-        #metodes datu apskatei
-def lasitaja_info(self):
-        print("lasitaja vārds: ", self.lasitaja_vards)
-        print("lasitaja uzvārds: ", self.lasitaja_uzvards)
-        print("lasitaja p.k.: ", self.lasitaja_pk)
-        print("lasitaja tel_nr: ", self.lasitaja_tel_nr)
-def gramatas_info(self):
-        print("gramatas raksturojums: ", self.gramatas_raksturojums)
-        print("gramatas nosaukums: ", self.gramatas_nosaukums)
-        print("gramatas cena: ", self.gramatas_cena)
+
+    #Izveidots konstruktors
+    def __init__(self,lasitajs_vards,lasitajs_uzvards,lasitajs_pk,lasitajs_tel_numurs,gramatas_kategorija,gramatas_raksturojums,gramatas_cena,gramatas_nosaukums):
+            self.lasitajs_vards = lasitajs_vards
+            self.lasitajs_uzvards = lasitajs_uzvards
+            self.lasitajs_pk = lasitajs_pk
+            self.lasitajs_tel_numurs = lasitajs_tel_numurs
+            self.gramatas_cena = gramatas_cena
+            self.gramatas_nosaukums = gramatas_nosaukums
+            self.gramatas_raksturojums = gramatas_raksturojums
+            self.gramatas_kategorija = gramatas_kategorija
+    #Izveidotas metodes datu apskatei
+    def lasitajs_info(self):
+        print("lasitajs vārds: ", self.lasitajs_vards)
+        print("lasitajs uzvārds: ", self.lasitajs_uzvards)
+        print("lasitajs p.k.: ", self.lasitajs_pk)
+        print("lasitajs tel numurs: ", self.lasitajs_tel_numurs)
+    def gramatas_info(self):
+        print("gramatas vārds: ", self.gramatas_nosaukums)
+        print("gramatas uzvārds: ", self.gramatas_raksturojums)
         print("gramatas kategorija: ", self.gramatas_kategorija)
-#Metodes datu saglabāšanai un logu veidošana
-def Pacients_saglabat(self):
+        print("gramatas cena: ", self.gramatas_cena)
+
+    #Izveidotas metodes datu saglabāšanai
+    def lasitajs_saglabat(self):
         with open('lasitajs.txt', 'w', encoding="utf-8") as fails :
             fails.write("-lasitajs-")
             fails.write(" \n")
-            fails.write(str(Dati.lasitaja_vards))
+            fails.write(str(Dati.lasitajs_vards))
             fails.write(" \n")
-            fails.write(str(Dati.lasitaja_uzvards))
+            fails.write(str(Dati.lasitajs_uzvards))
             fails.write(" \n")
-            fails.write(str(Dati.lasitaja_pk))
+            fails.write(str(Dati.lasitajs_pk))
             fails.write(" \n")
-            fails.write(str(Dati.lasitaja_tel_nr))
-def Dakteris_saglabat(self):
+            fails.write(str(Dati.lasitajs_tel_numurs))
+
+    def gramatas_saglabat(self):
         with open('gramatas.txt', 'w', encoding="utf-8") as fails :
             fails.write("-gramatas-")
             fails.write(" \n")
             fails.write(str(Dati.gramatas_nosaukums))
             fails.write(" \n")
-            fails.write(str(Dati.gramatu_kategorija))
+            fails.write(str(Dati.gramatas_raksturojums))
+            fails.write(" \n")
+            fails.write(str(Dati.gramatas_kategorija))
             fails.write(" \n")
             fails.write(str(Dati.gramatas_cena))
-            fails.write(" \n")
-            fails.write(str(Dati.gramatas_raksturojums))
-Dati = Biblioteka (lasitaja_vards="",lasitaja_uzvards="",lasitaja_pk="",lasitaja_tel_nr="",gramatas_raksturojums="",gramatas_nosaukums="",gramatas_cena="",gramatas_kategorija="")
+
+Dati = Biblioteka(lasitajs_vards="Jānis",lasitajs_uzvards="Kalniņš",lasitajs_pk="132435-32153",lasitajs_tel_numurs="25436789",gramatas_nosaukums=" Ronja un viņa laupītāju meita",gramatas_raksturojums="raksturots",gramatas_kategorija="Bērnu",gramatas_cena="25 EUR")
 Dati.lasitajs_saglabat()
 Dati.gramatas_saglabat()
 Dati.lasitajs_info()
@@ -54,31 +60,27 @@ Dati.gramatas_info()
 
 
 
-
 psg.theme('LightGreen4')
-
-
 layout = [
-        [psg.Text('Lasitajs')],
-        [psg.Text('Vards'),psg.InputText()],
-        [psg.Text('Uzvards'),psg.InputText()],
+        [psg.Text('LASĪTĀJS')],
+        [psg.Text('Vārds'),psg.InputText()],
+        [psg.Text('Uzvārds'),psg.InputText()],
         [psg.Text('Personas kods'),psg.InputText()],
         [psg.Text('Telefona numurs'),psg.InputText()],
-        [psg.Text('Nozare')],
-        [psg.Button('Saglabat lasitaja datus')],
+        [psg.Button('Saglabāt lasitaja datus')],
         [psg.Button('lasitaja datu apskate')]
 ]
-
-
 layout2 = [
-    [psg.Text("gramatas")],
-    [psg.Text('nosaukums'),psg.InputText()],
-    [psg.Text('Telefona numurs'),psg.InputText()],
-    [psg.Text('autors'),psg.InputText()],
-    [psg.Button('Saglabat gramatas datus')],
-    [psg.Button('gramatas datu apskate')]    
+    [psg.Text("GRĀMATAS")],
+    [psg.Text('Nosaukums'),psg.InputText()],
+    [psg.Text('Raksturojums'),psg.InputText()],
+    [psg.Text('Kategorija'),psg.InputText()],
+    [psg.Text('Cena'),psg.InputText()],
+    [psg.Button('Saglabāt grāmatas datus')],
+    [psg.Button('Grāmatas datu apskate')]    
     
 ]
+
 
 
 logugrupa = [[
@@ -86,12 +88,13 @@ logugrupa = [[
         [
          [
             psg.Tab('lasitajs',layout),
-            psg.Tab('gramata',layout2)
+            psg.Tab('gramatas',layout2)
          ]   
         ]
     ),
-    psg.Button('Aizvert')
+    psg.Button('Aizvērt')
 ]]
+
 
 window = psg.Window('Biblioteka', logugrupa)
 while True:
